b66f51f9e60dab7acda863b1fbe823b8b123198c
Wellersom <wellersom.wrv@gmail.com>
2026-05-31 17:50:27 -0300

    refactor: ajusta integracao da tela inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+
+defineOptions({
+  name: 'HomeView',
+})
+</script>
